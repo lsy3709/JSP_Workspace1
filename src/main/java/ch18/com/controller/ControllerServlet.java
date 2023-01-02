146383c9a5e56ae65d4114ffc037f7c11b264a43
lsy3709 <lsy3709@naver.com>
2023-01-02 11:51:36 +0900

    230102 3교시 저장1

--- src/main/java/ch18/com/controller/ControllerServlet.java
+++ src/main/java/ch18/com/controller/ControllerServlet.java
@@ -22,15 +22,29 @@ public class ControllerServlet extends HttpServlet {
 		String id = request.getParameter("id");
 		String password = request.getParameter("passwd");
 
+		//DTO 형식 처럼 사용되는 객체.
+		// 입력 받은 id , 패스워드를 임시로 저장 해두는 역할.
 		LoginBean bean = new LoginBean();
 		bean.setId(id);
 		bean.setPassword(password);
+		
+		// request 내장객체 key: bean , value : bean 실제 객체가 들어 있음. 
 		request.setAttribute("bean", bean);
 
+		// bean 객체  내부에 validate() 메서드가 있음. 
+		// 하드 코딩이고, 패스워드: admin 으로 하면, true 반환 메서드.
 		boolean status = bean.validate();
 
+		// 간단한 유효성 체크를 통과한 후. 뷰에 값을 전달하는 역할. 
 		if (status) {
+			// mvc_success.jsp 뷰 페이지를 rd 라는 객체에 담아서. 
+			// 전달하는 역할. 
+			// 뷰로 전달한다.  (request , response 두개의 객체를 전달합니다.)
+			// 그러면 뷰에서 해당 객체에 담겨진 데이터를 꺼내서 사용하는 형식. 
 			RequestDispatcher rd = request.getRequestDispatcher("mvc_success.jsp");
+			// 결과 
+			// 웹브라우저에서는 해당 뷰페이지의 정보만 불러오게되고.
+			// 해당 URL 주소는 변화가 없음. 
 			rd.forward(request, response);
 		} else {
 			RequestDispatcher rd = request.getRequestDispatcher("mvc_error.jsp");
--- src/main/webapp/ch18/mvc.jsp
+++ src/main/webapp/ch18/mvc.jsp
@@ -4,6 +4,14 @@
 <title>MVC</title>
 </head>
 <body>
+<!-- 뷰에서 입력된 정보를 , 전송 버튼을 누르게 되면, 
+action="ControllerServlet" 여기서 처리를 합니다. 
+현재, 뷰 페이지의 위치는 /ch18/
+<servlet-mapping>
+		 <servlet-name>myController</servlet-name>
+ 		<url-pattern>/ch18/ControllerServlet</url-pattern>
+	 </servlet-mapping> -->
+	 
 	<form method="post" action="ControllerServlet">
 		<p>	아이디 : <input type="text" name="id">
 		<p>	비밀번호 : <input type="password" name="passwd">
